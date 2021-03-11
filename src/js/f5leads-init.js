9ef32f11f1f8a1fc569f651ac82d394359fba4b3
Belov Roman <rnbelov96@gmail.com>
2021-03-11 19:01:24 +0300

    Настраивает почту для отправки писем

--- src/js/f5leads-init.js
+++ src/js/f5leads-init.js
@@ -1,5 +1,5 @@
 document.f5leads = {};
-document.f5leads.emails = 'oidars@gmail.com'; // comma separated
+document.f5leads.emails = 'franch@labz4.ru,leads@conversionpro.agency'; // comma separated
 document.f5leads.send_to_bitrix = 'false';
 document.f5leads.expect_second_form = '0';
 document.f5leads.counterId = '';
